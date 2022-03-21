f266fa794cccedba7042fefadfcd24b302e0163e
grsjob <92042203+grsjob@users.noreply.github.com>
2022-03-21 16:02:01 +0300

    Initial commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MainPage from "/src/components/pages/main-page/main-page";
+import ScrollTop from "/src/components/ui/scroll-top/scroll-top";
+import PageWrapper from "/src/components/layout/page-wrapper/page-wrapper";
+import Order from "/src/components/pages/order/order";
+import { AppRoute } from "/src/const";
+import features from "/src/mocks/features";
+import products from "/src/mocks/products";
+import { GlobalStyle } from "./styles";
+
+// Корневой компонент всего приложения
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <ScrollTop />
+        <Switch>
+          <Route exact path={AppRoute.MAIN}>
+            <PageWrapper pageUrl={AppRoute.MAIN}>
+              <MainPage features={features} />
+            </PageWrapper>
+          </Route>
+          <Route exact path={AppRoute.ORDER}>
+            <PageWrapper pageUrl={AppRoute.ORDER}>
+              <Order products={products} />
+            </PageWrapper>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/layout/nav/nav.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "/src/components/ui/button/button";
+import { Ul, Li } from "/src/components/styled";
+import { AppRoute } from "/src/const";
+import { StyledButton } from "./styles";
+
+const links = [
+  {
+    to: AppRoute.MAIN,
+    item: <StyledButton link={AppRoute.MAIN}>Главная</StyledButton>
+  },
+  { to: AppRoute.ORDER, item: <Button link={AppRoute.ORDER}>Купить</Button> }
+];
+
+// навигация
+function Nav({ pageUrl }) {
+  return (
+    <nav>
+      <Ul>
+        {links
+          .filter((link) => link.to !== pageUrl)
+          .map((link) => (
+            <Li key={link.to}>{link.item}</Li>
+          ))}
+      </Ul>
+    </nav>
+  );
+}
+
+export default Nav;
